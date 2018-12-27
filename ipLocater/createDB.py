9d9b8cffdc98b1cfed8cd9ea949b9e20bbd054f8
abertossa <alec.bertossa@libertyits.com>
2018-12-27 05:19:09 -0500

    can create local db, still figuring out requirements for heroku

---
+++ ipLocater/createDB.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# make this less hardcoded
+file='../resources/test-ipv4.csv'
+
+print(pd.read_csv(file, nrows=5))
+csv_database = create_engine('sqlite:///ip_addresses.db')
+
+CHUNKSIZE = 1000
+fields=['network', 'latitude', 'longitude']
+
+# not using the fields other than lat and long right now. saving the network just in case i want to use it
+for df in pd.read_csv(file, chunksize=CHUNKSIZE, iterator=True, usecols=fields):
+    df.to_sql('insert_table', csv_database, if_exists='append')
